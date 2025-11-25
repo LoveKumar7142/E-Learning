0539bd8c1bc63db3783b5738949fd5f93b7c8125
Love Kumar <lovekumar714283@gmail.com>
2025-11-25 23:59:51 +0530

    delete courses and lactures in admin controller and admin stats check

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,7 +1,10 @@
+import { promisify } from "util";
 import TryCatch from "../middlewares/TryCatch.js";
 import { Courses } from "../models/caurse.model.js";
 import { Lacture } from "../models/Lacture.model.js";
 import { rm } from "fs"
+import fs from "fs"
+import { User } from "../models/user.model.js";
 
 export const createCourse = TryCatch(async (req,res) => {
     const {title,description,category,createdBy,duration,price} = req.body
@@ -52,8 +55,61 @@ export const addLectures = TryCatch(
     }
 )
 
+
+const unlikeAsync = promisify(fs.unlink)
 export const deleteLacture = TryCatch(async (req,res) => {
     const lecture = await Lacture.findById(req.params.id)
+    rm(lecture.video,()=>{
+        console.log("Video Lacture Deleted");
+    })
 
-    
+    await lecture.deleteOne()
+
+    res.json({message:"Lacture Deleted Successfully"})
+})
+
+export const deleteCourse = TryCatch(async (req,res) => {
+    const course = await Courses.findById(req.params.id)
+
+
+    const lectures = await Lacture.find({course: course._id})
+
+    await Promise.all(
+        lectures.map(async(lacture) => {
+            await unlikeAsync(lacture.video)
+            console.log("Video Deleted")
+        })
+
+    )
+
+    rm(course.image,() => {
+        console.log("image Deleted")
+    })
+
+    await Lacture.find({course: req.params.id}).deleteMany()
+
+    await course.deleteOne()
+
+    await User.updateMany({},{$pull:{subscription:req.params.id}})
+
+    res.json({
+        message:"Course Deleted",
+    })
+})
+
+
+export const getAllStats = TryCatch(async (req,res) => {
+    const totalCourses = (await Courses.find()).length
+
+    const totalLectures = (await Lacture.find()).length
+    const totalUser = (await User.find()).length
+    const stats = {
+        totalCourses,
+        totalLectures,
+        totalUser
+    }
+
+    res.json({
+        stats
+    })
 })
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuth } from "../middlewares/isAuth.js";
-import { addLectures, createCourse } from "../controllers/admin.controller.js";
+import { addLectures, createCourse, deleteCourse, deleteLacture, getAllStats } from "../controllers/admin.controller.js";
 import { uploadFiles } from "../middlewares/multer.js";
 
 const router = express.Router();
@@ -8,6 +8,9 @@ const router = express.Router();
 router.post("/course/new", uploadFiles, isAuth, isAdmin, createCourse);
 
 router.post("/course/:id",isAuth,isAdmin,uploadFiles,addLectures)
+router.delete("/course/:id",isAuth,isAdmin,deleteCourse)
+router.delete("/lacture/:id",isAuth,isAdmin,deleteLacture)
+router.get("/stats",isAuth,isAdmin,getAllStats)
 
 
 export default router;
